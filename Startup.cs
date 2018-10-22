0ed5b9605182030d975232525438d5c93f6c70f8
Alexander Kotov <alexanderko@interlink-ua.com>
2018-10-23 01:53:33 +0300

    disable ssl

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace TodoSpa
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
