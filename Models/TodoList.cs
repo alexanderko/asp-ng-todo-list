c8b81fbefd01cd643241ae8a139be8d06445535c
Alexander Kotov <alexanderko@interlink-ua.com>
2018-10-31 15:44:24 +0200

    API: log EF generated SQL queries

--- Models/TodoList.cs
+++ Models/TodoList.cs
@@ -6,8 +6,10 @@ namespace TodoSpa.Models
     public class TodoList
     {
         public int Id { get; set; }
+
         [Required]
         public string Title { get; set; }
+
 
         public List<Task> Tasks { get; set; }
     }
